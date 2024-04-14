27ae3a71ad7e7fd4261126db67b19b6186df2237
Nadia-kaf <nadiabokordedzi@gmail.com>
2024-04-14 22:25:15 +0000

    login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,38 +1,40 @@
+import reception from "../assets/recption.jpg"
 
 
 export default function Login(){
     return(
-        <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="min-h-screen  py-6 flex flex-col justify-center sm:py-12  "  style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)),url(${reception})`, backgroundSize: 'cover', backgroundPosition: 'center', height:"280px" }}>
+      
+  <div className="flex py-3 mb-40 mt-40  sm:max-w-l sm:mx-auto " >
     <div
-      class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+      className="absolute inset-0 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
     </div>
-    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+    <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 mr-40 ml-20">
 
-      <div class="max-w-md mx-auto">
+      <div className="max-w-md mx-auto">
         <div>
-          <h1 class="text-2xl font-semibold">Login</h1>
+          <h1 className="text-2xl font-semibold">Log In as a Client</h1>
         </div>
-        <div class="divide-y divide-gray-200">
-          <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-            <div class="relative">
-              <input autocomplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
-              <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+        <div className="divide-y divide-gray-200">
+          <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+            <div className="relative">
+              <input autocomplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+              <label for="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
             </div>
-            <div class="relative">
-              <input autocomplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-              <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+            <div className="relative">
+              <input autocomplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+              <label for="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
             </div>
-            <div class="relative">
-              <button class="bg-cyan-500 text-white rounded-md px-2 py-1">Submit</button>
+            <div className="relative">
+              <button className="bg-cyan-500 text-white rounded-md px-2 py-1">Submit</button>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="w-full flex justify-center">
-        <button class="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-        <svg class="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
+      <div className="w-full flex justify-center">
+        <button className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+        <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
                         <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
                         <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
                         <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path>
@@ -41,15 +43,25 @@ export default function Login(){
           <span>Log in with Google</span>
         </button>
       </div>
-      <div class="flex gap-2 pt-2 mt-2">
-          <p class="text-gray-600 text-sm">Don't have an account as a User?</p><a class="text-gray-600 text-sm underline"
+      <div className="flex gap-2 pt-2 mt-2">
+          <p className="text-gray-600 text-sm">Don't have an account as a Client?</p><a className="text-gray-600 text-sm underline"
             href="/signup">Sign up</a>
         </div>
-            <div class="flex gap-2 pt-2">
-          <p class="text-gray-600 text-sm">Don't have an account as  Health Professional?</p><a class="text-gray-600 text-sm underline"
+            <div className="flex gap-2 pt-2">
+          <p className="text-gray-600 text-sm">Don't have an account as  Health Professional?</p><a className="text-gray-600 text-sm underline"
             href="/signupH">sign up here</a>
         </div>  
     </div>
+    <div className="flex items-center ml-20 justify-end mb-44">
+      <h1 className="text-white border-b-2 border-blueGray-300 text-xl">Ready to book medical appointment?
+      </h1>
+    </div>
+   
+    <div className=" items-center justify-end mb-40">
+      <h1 className="text-white border-b-2  border-blueGray-300 text-2xl">Welcome Back to BookMedics
+      </h1>
+    </div>
+   
   </div>
 </div>
     )
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -6,18 +6,18 @@ export default function SignUp() {
         <div className="relative px-4 py-5 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <div>
-              <h1 className="text-2xl font-semibold">Register</h1>
+              <h1 className="text-2xl font-semibold">Register as a Client</h1>
             </div>
             <div className="divide-y divide-gray-200">
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <div classNameName="flex gap-10">
+                <div classNameName="flex">
                   <div className="relative">
                     <input
                       autocomplete="off"
                       id="email"
                       name="email"
                       type="text"
-                      className="peer placeholder-transparent h-10 w-20 border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      className="peer placeholder-transparent h-10 w-20 border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 mb-5"
                       placeholder="Email address"
                     />
                     <label
